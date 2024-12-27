5b7e0b5be9ec57557911911efc9b482f0ac45c2b
Millat Hosen <codermillat@gmail.com>
2024-12-27 22:59:13 +0600

    Updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,11 @@
 import React from 'react';
-import { Routes, Route } from 'react-router-dom';
+import { 
+  Routes, 
+  Route, 
+  createRoutesFromElements, 
+  createBrowserRouter,
+  RouterProvider 
+} from 'react-router-dom';
 import AppProvider from './providers/app/AppProvider';
 import TelegramAuthCheck from './components/TelegramAuthCheck';
 import MainLayout from './layouts/MainLayout';
@@ -10,19 +16,29 @@ import Friends from './pages/Friends';
 import Earn from './pages/Earn';
 import Admin from './pages/Admin';
 
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route element={<MainLayout />}>
+      <Route path="/" element={<Home />} />
+      <Route path="/tasks" element={<Tasks />} />
+      <Route path="/leaderboard" element={<Leaderboard />} />
+      <Route path="/friends" element={<Friends />} />
+      <Route path="/earn" element={<Earn />} />
+      <Route path="/admin" element={<Admin />} />
+    </Route>
+  ),
+  {
+    future: {
+      v7_startTransition: true,
+      v7_relativeSplatPath: true
+    }
+  }
+);
+
 const App: React.FC = () => (
   <AppProvider>
     <TelegramAuthCheck>
-      <MainLayout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/tasks" element={<Tasks />} />
-          <Route path="/leaderboard" element={<Leaderboard />} />
-          <Route path="/friends" element={<Friends />} />
-          <Route path="/earn" element={<Earn />} />
-          <Route path="/admin" element={<Admin />} />
-        </Routes>
-      </MainLayout>
+      <RouterProvider router={router} />
     </TelegramAuthCheck>
   </AppProvider>
 );
